0ba5661cef6b65675daec6d527e7cea8f7a368f9
saumyasrivastava226 <somu.22102000@gmail.com>
2024-04-03 08:45:57 +0530

    project structure and initial code

---
+++ src/pages/Registration.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+
+export default function Registration(){
+    const[name,setName]= useState();
+    const [username,setUserName]=useState();
+    const[email,setEmail]=useState();
+    const[mobile,setMobile]=useState();
+    const[consent,setConsent]=useState(false);
+        
+
+    return (
+        // We are going to need state variables
+      
+        <div>Registration</div>
+    )
+}
